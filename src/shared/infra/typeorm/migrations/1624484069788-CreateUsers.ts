1c8c05c1743a2648c7a9467fc0d5637e7b083e4f
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-23 20:34:46 -0300

    add migrations de Users e ajustes no typeorm

--- src/shared/infra/typeorm/migrations/1624484069788-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1624484069788-CreateUsers.ts
@@ -1,11 +1,70 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUsers1624484069788 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "username",
+                        type: "varchar",
+                        isUnique: true,
+                    },
+                    {
+                        name: "password",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        isUnique: true,
+                    },
+                    {
+                        name: "avatar",
+                        type: "varchar",
+                        isNullable: true,
+                    },
+                    {
+                        name: "isAdmin",
+                        type: "boolean",
+                        default: false
+                    },
+                    {
+                        name: "type_user",
+                        type: "varchar",
+                        
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+
+                ]
+            })
+        )
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
     }
 
 }
